4a3630152078698558d9a75e36c20e271bff6a1b
xdJidx <bernardjerome59@gmail.com>
2023-09-27 14:11:20 +0200

    Add 1-hbnb file html, py and js and replace scipts directory

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  // Define an object to store selected amenities
+  const selectedAmenities = {};
+
+  // Define the maximum number of characters to display in the h4
+  const maxCharacters = 30; // Change this value as needed
+
+  // Listen for changes on each input checkbox tag
+  $('input[type="checkbox"]').change(function() {
+    // Get Amenity ID and Name from data attributes
+    const amenityName = $(this).data('name');
+
+    // Check if the checkbox is checked
+    if ($(this).is(':checked')) {
+      // Store Amenity Name in the selectedAmenities object
+      selectedAmenities[amenityName] = true;
+    } else {
+      // Remove Amenity Name from the selectedAmenities object
+      delete selectedAmenities[amenityName];
+    }
+
+    // Update the h4 tag inside the Amenities div with the list of checked Amenities
+    updateAmenitiesList(selectedAmenities);
+  });
+
+  // Function to update the amenities list
+  function updateAmenitiesList(amenities) {
+    const amenityNames = Object.keys(amenities);
+
+    // Join the names with a comma
+    let displayText = amenityNames.join(', ');
+
+    // Check if the text exceeds the maximum number of characters
+    if (displayText.length > maxCharacters) {
+      // Trim the text to the maximum number of characters and add "..."
+      displayText = displayText.substring(0, maxCharacters - 3) + '...';
+    }
+
+    $('.amenities h4').text(displayText);
+  }
+});
