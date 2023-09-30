372c7fdd33e8b06c55124bd3fdf4095c4416bb69
xdJidx <bernardjerome59@gmail.com>
2023-09-30 16:30:00 +0200

    Add update script function fetchPlaces

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,4 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // Function to update the API status
+  function updateApiStatus() {
+      // Make an HTTP GET request to the API status endpoint
+      $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+          console.log(data); // Affiche la réponse de l'API dans la console.
+          // Check if the status is "OK"
+          if (data.status === "OK") {
+              // Add the class "available" to the div#api_status
+              $('#api_status').addClass('available');
+              console.log(data); // Affiche la réponse de l'API dans la console
+          } else {
+              // Remove the class "available" from the div#api_status
+              $('#api_status').removeClass('available');
+          }
+      });
+  }
+
   // Function to update the amenities list
   function updateAmenitiesList(amenities) {
       const amenityNames = Object.keys(amenities);
@@ -15,54 +32,48 @@ $(document).ready(function() {
       $('.amenities h4').text(displayText);
   }
 
-  function loadPlaces() {
+  // Function to fetch and display places
+  function fetchPlaces() {
       $.ajax({
           type: 'POST',
-          url: '/api/v1/places_search',
+          url: 'http://0.0.0.0:5001/api/v1/places_search/',
           contentType: 'application/json',
           data: JSON.stringify({}),
-          success: function(data) {
-              $('.places').empty();
-
-              data.forEach(function(place) {
+          success: function (data) {
+            console.log(data)
+              // Loop through the result of the request
+              data.forEach(function (place) {
+                  // Create an article tag representing a Place
                   const article = $('<article>');
-                  article.html(`
-                      <div class="title_box">
-                          <h2>${place.name}</h2>
+                  article.html(
+                      `<div class="title_box">
+                        <h2>${place.name}</h2>
                           <div class="price_by_night">$${place.price_by_night}</div>
                       </div>
                       <div class="information">
-                          <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
-                          <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
-                          <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+                        <div class="max_guest">
+                          ${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}
+                        </div>
+                        <div class="number_rooms">
+                          ${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}
+                        </div>
+                        <div class="number_bathrooms">
+                          ${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}
+                        </div>
                       </div>
-                      <div class="description">${place.description}</div>
-                  `);
-                  $('.places').append(article);
+                      <div class="description">${place.description}</div>`
+                  );
+                  $('section.places').append(article);
               });
+          },
+          error: function (xhr, status, error) {
+              console.error('Error fetching places:', error);
           }
       });
   }
 
-  // Function to update the API status
-  function updateApiStatus() {
-      // Make an HTTP GET request to the API status endpoint
-      $.get('/api/v1/status', function(data) {
-          console.log(data); // Display the API response in the console.
-          // Check if the status is "OK"
-          if (data.status === "OK") {
-              // Add the class "available" to the div#api_status
-              $('#api_status').addClass('available');
-          } else {
-              // Remove the class "available" from the div#api_status
-              $('#api_status').removeClass('available');
-          }
-      });
-  }
-
-  // Initial update of API status and load places
+  // Initial update of API status
   updateApiStatus();
-  loadPlaces();
 
   // Set an interval to periodically update the API status (e.g., every 10 seconds)
   setInterval(updateApiStatus, 10000); // Update every 10 seconds
@@ -74,7 +85,7 @@ $(document).ready(function() {
   const maxCharacters = 30; // Change this value as needed
 
   // Listen for changes on each input checkbox tag
-  $('input[type="checkbox"]').change(function() {
+  $('input[type="checkbox"]').change(function () {
       // Get Amenity ID and Name from data attributes
       const amenityName = $(this).data('name');
 
@@ -93,4 +104,7 @@ $(document).ready(function() {
 
   // Initial update of the amenities list when the page loads
   updateAmenitiesList(selectedAmenities);
+
+  // Call the function to fetch and display places
+  fetchPlaces();
 });
